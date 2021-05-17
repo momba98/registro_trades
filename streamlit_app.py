928ab1de0594385136d0c4b339d34e15ed249781
Andre Mombach <1998amds@gmail.com>
2021-05-17 20:40:24 -0300

    updt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image
 import os
+from pydrive.drive import GoogleDrive
+from pydrive.auth import GoogleAuth
 
 st.set_page_config(
     page_title='Teste Drive',
@@ -25,20 +27,15 @@ def save_uploadedfile(uploadedfile):
      return st.success("Saved File:{} to tempDir".format(uploadedfile.name))
 
 if image_file is not None:
+
     save_uploadedfile(image_file)
 
     st.image(f'tempDir/{image_file.name}',use_column_width=True)
 
-    from pydrive.auth import GoogleAuth
-
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth()
-
-    from pydrive.drive import GoogleDrive
-
     drive = GoogleDrive(gauth)
 
     file5 = drive.CreateFile()
-    # Read file and set it as a content of this instance.
     file5.SetContentFile(f'tempDir/{image_file.name}')
-    file5.Upload() # Upload the file.
+    file5.Upload()
